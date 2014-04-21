97e22027a4928a4a67abe70b3db56bf07f6978dd
Ingo Muschenetz <ingo@muschenetz.com>
2014-04-21 16:43:47 -0700

    Corrected deployment script

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -2,21 +2,25 @@
 
 # Make backup folder
 mkdir $MODULE_ROOT/dist_bak
-cp $MODULE_ROOT/ios/*.zip $MODULE_ROOT/dist_bak
-cp $MODULE_ROOT/android/dist/*.zip $MODULE_ROOT/dist_bak
+cp -fv $MODULE_ROOT/ios/*.zip $MODULE_ROOT/dist_bak
+cp -fv $MODULE_ROOT/android/dist/*.zip $MODULE_ROOT/dist_bak
 
 # Make destination folder
-mkdir $MODULE_ROOT/dist
-NAME="${FILENAME%.*}"
-EXT="${FILENAME##*.}"
-NOW=$(date +"%d%m%Y")
+mkdir $MODULE_ROOT/dist_upload
+NOW=$(date +"%Y%m%d%H%M")
 
 # Copy files with changed names
-for FILENAME in $MODULE_ROOT/dist_bak/*.zip
+cd $MODULE_ROOT/dist_bak
+for FILENAME in *.zip
 do
-  cp "${FILENAME}" "$MODULE_ROOT/dist/${NAME}-${NOW}.${EXT}"
+  echo "${FILENAME}"
+  cp -fv "${FILENAME}" "${MODULE_ROOT}/dist_upload/${FILENAME%.*}-${NOW}.${FILENAME##*.}"
 done
 
 # Upload to S3
-cd $MODULE_ROOT/dist
-travis-artifacts upload --path *.zip --target-path modules/$MODULE_NAME/"
+cd $MODULE_ROOT/dist_upload
+# Yes, you need to call this once per platform
+travis-artifacts upload --path *android*.zip --target-path modules/$MODULE_NAME/
+travis-artifacts upload --path *iphone*.zip --target-path modules/$MODULE_NAME/
+rm -rf dist_bak
+rm -rf dist_upload
