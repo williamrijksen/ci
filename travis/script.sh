8a820dbd55a48051be526b5907cd49e0438d721b
Ingo Muschenetz <ingo@muschenetz.com>
2014-04-24 15:12:55 -0700

    Create lib folder if necessary

--- travis/script.sh
+++ travis/script.sh
@@ -10,6 +10,12 @@ fi
 if [ -d "$MODULE_ROOT/android/" ]; then
   cd $MODULE_ROOT/android/
   cp $MODULE_ROOT/build.properties build.properties
+
+  # if lib folder doesn't exist, create it
+  if [ -d "lib" ]; then
+    mkdir lib
+  fi
+
   ant clean
   ant
 fi
