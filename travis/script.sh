fcc11af7f46d44d7e70879353f1e676cb9d730e1
Ingo Muschenetz <ingo@muschenetz.com>
2014-04-29 15:52:49 -0700

    Added output for platforms

--- travis/script.sh
+++ travis/script.sh
@@ -2,12 +2,23 @@
 
 # If iOS module exists, build
 if [ -d "$MODULE_ROOT/ios/" ]; then
+
+  echo
+  echo "Building iOS version"
+  echo
+
   cd $MODULE_ROOT/ios/
   ./build.py
+
 fi
 
 # if Android module exists, build
 if [ -d "$MODULE_ROOT/android/" ]; then
+
+  echo
+  echo "Building Android version"
+  echo
+
   cd $MODULE_ROOT/android/
   cp $MODULE_ROOT/build.properties build.properties
 
@@ -16,4 +27,7 @@ if [ -d "$MODULE_ROOT/android/" ]; then
 
   ant clean
   ant
+
+  cd $MODULE_ROOT
+  
 fi
