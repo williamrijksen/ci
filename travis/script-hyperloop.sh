e303bbee131e8d55a263eb5c7efb3c571fa7102a
Ingo Muschenetz <ingo@muschenetz.com>
2014-06-05 16:17:31 -0700

    Explicitly use titanium SDK directory

--- travis/script-hyperloop.sh
+++ travis/script-hyperloop.sh
@@ -4,6 +4,6 @@
 git clone https://github.com/appcelerator/titanium_mobile.git $TRAVIS_BUILD_DIR/titanium_mobile
 export TI_ROOT=$TRAVIS_BUILD_DIR/titanium_mobile
 
-hyperloop package module --platform=ios --src=$MODULE_ROOT/ios --dest=$MODULE_ROOT/ios/build --debug
+hyperloop package module --platform=ios --src=$MODULE_ROOT/ios --dest=$MODULE_ROOT/ios/build --tisdk=$TRAVIS_BUILD_DIR/titanium_mobile --debug
 
 ls -la
