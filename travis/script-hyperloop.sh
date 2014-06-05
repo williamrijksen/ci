2e0ebe61bf4223d25af8be988a2a9f6c4f60dfaf
Ingo Muschenetz <ingo@muschenetz.com>
2014-06-05 15:11:19 -0700

    Update script-hyperloop.sh

--- travis/script-hyperloop.sh
+++ travis/script-hyperloop.sh
@@ -4,6 +4,6 @@
 git clone https://github.com/appcelerator/titanium_mobile.git
 TI_ROOT=./titanium_mobile
 
-hyperloop package module --platform=ios --src=$MODULE_ROOT/ios --dest=$MODULE_ROOT/ios/build —debug
+hyperloop package module --no-apidoc --platform=ios --src=$MODULE_ROOT/ios --dest=$MODULE_ROOT/ios/build —debug
 
 ls -la
