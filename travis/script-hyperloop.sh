32f8a20cf2d6a3d1bda65638a0136cc0dc82087d
Ingo Muschenetz <ingo@muschenetz.com>
2014-06-05 11:40:34 -0700

    Fixed root folder links.

--- travis/script-hyperloop.sh
+++ travis/script-hyperloop.sh
@@ -12,12 +12,13 @@ python ./titanium_mobile/apidoc/docgen.py -f modulehtml -o $MODULE_ROOT/build/ap
 # Add additional folders to the module zip
 # Make sure to change the version numbers below to match the current version of the module
 
-mkdir -p $MODULE_ROOT/modules/iphone/$MODULE_NAME/$MODULE_VERSION
-cp -RP $MODULE_ROOT/build/apidoc $MODULE_ROOT/modules/iphone/$MODULE_NAME/$MODULE_VERSION/
-cp -RP $MODULE_ROOT/example $MODULE_ROOT/modules/iphone/$MODULE_NAME/$MODULE_VERSION/
-cp -RP $MODULE_ROOT/documentation modules/iphone/$MODULE_NAME/$MODULE_VERSION/
-cp $MODULE_ROOT/build/$MODULE_NAME-iphone-1.0.0.zip $MODULE_ROOT/$MODULE_NAME-iphone-$MODULE_VERSION.zip
-zip --symlinks -r $MODULE_ROOT/$MODULE_NAME-iphone-$MODULE_VERSION.zip $MODULE_ROOT/modules/ -x \*DS_Store
+ROOT_FOLDER=$MODULE_ROOT/ios
+mkdir -p $ROOT_FOLDER/modules/iphone/$MODULE_NAME/$MODULE_VERSION
+cp -RP $ROOT_FOLDER/build/apidoc $ROOT_FOLDER/modules/iphone/$MODULE_NAME/$MODULE_VERSION/
+cp -RP $ROOT_FOLDER/example $ROOT_FOLDER/modules/iphone/$MODULE_NAME/$MODULE_VERSION/
+cp -RP $ROOT_FOLDER/documentation $ROOT_FOLDER/modules/iphone/$MODULE_NAME/$MODULE_VERSION/
+cp $ROOT_FOLDER/build/$MODULE_NAME-iphone-1.0.0.zip $ROOT_FOLDER/$MODULE_NAME-iphone-$MODULE_VERSION.zip
+zip --symlinks -r $ROOT_FOLDER/$MODULE_NAME-iphone-$MODULE_VERSION.zip $ROOT_FOLDER/modules/ -x \*DS_Store
 
 # Cleanup
-rm -rf $MODULE_ROOT/modules
+rm -rf $ROOT_FOLDER/modules
