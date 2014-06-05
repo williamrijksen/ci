83b3d927ced185d3614f5fa10b8f540a9e68bda6
Ingo Muschenetz <ingo@muschenetz.com>
2014-06-05 15:24:38 -0700

    Added doc building back in

--- travis/script-hyperloop.sh
+++ travis/script-hyperloop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # create a local cloned repo
-git clone https://github.com/appcelerator/titanium_mobile.git
-TI_ROOT=./titanium_mobile
+git clone https://github.com/appcelerator/titanium_mobile.git $TRAVIS_BUILD_DIR/titanium_mobile
+export TI_ROOT=$TRAVIS_BUILD_DIR/titanium_mobile
 
-hyperloop package module --no-apidoc --platform=ios --src=$MODULE_ROOT/ios --dest=$MODULE_ROOT/ios/build —debug
+hyperloop package module --platform=ios --src=$MODULE_ROOT/ios --dest=$MODULE_ROOT/ios/build —debug
 
 ls -la
