038cdd79eea1ecd54d699a578ce4e67a5ca55f4d
Ingo Muschenetz <ingo@muschenetz.com>
2014-06-05 17:21:38 -0700

    Echo out the hyperloop command

--- travis/script-hyperloop.sh
+++ travis/script-hyperloop.sh
@@ -2,8 +2,6 @@
 
 # create a local cloned repo
 git clone https://github.com/appcelerator/titanium_mobile.git $TRAVIS_BUILD_DIR/titanium_mobile
-export TI_ROOT=$TRAVIS_BUILD_DIR/titanium_mobile
 
+echo "hyperloop package module --platform=ios --src=$MODULE_ROOT/ios --dest=$MODULE_ROOT/ios/build --tisdk=$TRAVIS_BUILD_DIR/titanium_mobile --debug"
 hyperloop package module --platform=ios --src=$MODULE_ROOT/ios --dest=$MODULE_ROOT/ios/build --tisdk=$TRAVIS_BUILD_DIR/titanium_mobile --debug
-
-ls -la
