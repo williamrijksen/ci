1af6682de92b5ca81ea772fa88bfbe98f6656dfd
Ingo Muschenetz <ingo@muschenetz.com>
2014-06-05 21:16:12 -0700

    Removed setting of SDK location

--- travis/script-hyperloop.sh
+++ travis/script-hyperloop.sh
@@ -4,4 +4,4 @@
 git clone https://github.com/appcelerator/titanium_mobile.git $TRAVIS_BUILD_DIR/titanium_mobile
 
 echo "hyperloop package module --platform=ios --src=$MODULE_ROOT/ios --dest=$MODULE_ROOT/ios/build --tisdk=$TRAVIS_BUILD_DIR/titanium_mobile --debug"
-hyperloop package module --platform=ios --src=$MODULE_ROOT/ios --dest=$MODULE_ROOT/ios/build --tisdk=$TRAVIS_BUILD_DIR/titanium_mobile --debug
+hyperloop package module --platform=ios --src=$MODULE_ROOT/ios --dest=$MODULE_ROOT/ios/build --debug
