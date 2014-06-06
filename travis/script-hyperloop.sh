822bfc66c36eb432b643f4029dfd8f62ba53024f
Ingo Muschenetz <ingo@muschenetz.com>
2014-06-05 21:49:50 -0700

    Added in Python pre-reqs

--- travis/script-hyperloop.sh
+++ travis/script-hyperloop.sh
@@ -4,5 +4,10 @@
 git clone https://github.com/appcelerator/titanium_mobile.git $TRAVIS_BUILD_DIR/titanium_mobile
 export TI_ROOT=$TRAVIS_BUILD_DIR/titanium_mobile
 
+# install doc pre-requisites
+sudo easy_install pyyaml
+sudo easy_install Pygments
+ 
+# compile module
 echo "hyperloop package module --platform=ios --src=$MODULE_ROOT/ios --dest=$MODULE_ROOT/ios/build --tisdk=$TRAVIS_BUILD_DIR/titanium_mobile --debug"
 hyperloop package module --platform=ios --src=$MODULE_ROOT/ios --dest=$MODULE_ROOT/ios/build --debug
