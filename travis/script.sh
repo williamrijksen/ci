c1764696b3ef58f8b93f117d327303eb936073d5
Ayo Adesugba <ayoadesugba@gmail.com>
2014-07-28 19:34:12 -0400

    Fix ci.travis - Return status and Android SDK.

--- travis/script.sh
+++ travis/script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+RETSTATUS=0
+STATUS=0
+
 # If iOS module exists, build
 if [ -d "$MODULE_ROOT/ios/" ]; then
 
@@ -10,6 +13,10 @@ if [ -d "$MODULE_ROOT/ios/" ]; then
   cd $MODULE_ROOT/ios/
   ./build.py
 
+  let STATUS=$?
+  if (( "$RETSTATUS" == "0" )) && (( "$STATUS" != "0" )); then
+    let RETSTATUS=$STATUS
+  fi
 fi
 
 # if Android module exists, build
@@ -21,13 +28,21 @@ if [ -d "$MODULE_ROOT/android/" ]; then
 
   cd $MODULE_ROOT/android/
   cp $MODULE_ROOT/build.properties build.properties
-
+  cat build.properties
+  
   # if lib folder doesn't exist, create it
   mkdir -p lib
 
   ant clean
   ant
 
+  let STATUS=$?
+  if (( "$RETSTATUS" == "0" )) && (( "$STATUS" != "0" )); then
+    let RETSTATUS=$STATUS
+  fi
+
   cd $MODULE_ROOT
   
 fi
+
+exit $RETSTATUS
