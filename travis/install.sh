40008a4e659338188ec98b39b311271616ac9b26
Ingo Muschenetz <ingo@muschenetz.com>
2014-10-03 14:53:58 -0700

    Append to, don't overwrite the file

--- travis/install.sh
+++ travis/install.sh
@@ -133,9 +133,9 @@ if [ -d "$MODULE_ROOT/ios/" ]; then
   # Write out properties file
  
   echo "TITANIUM_SDK = $TITANIUM_SDK" > $MODULE_ROOT/titanium.xcconfig
-  echo "TITANIUM_BASE_SDK = '$(TITANIUM_SDK)/iphone/include'" > $MODULE_ROOT/titanium.xcconfig
-  echo "TITANIUM_BASE_SDK2 = '$(TITANIUM_SDK)/iphone/include/TiCore'" > $MODULE_ROOT/titanium.xcconfig
-  echo "HEADER_SEARCH_PATHS= $(TITANIUM_BASE_SDK) $(TITANIUM_BASE_SDK2)" > $MODULE_ROOT/titanium.xcconfig
+  echo "TITANIUM_BASE_SDK = '$(TITANIUM_SDK)/iphone/include'" >> $MODULE_ROOT/titanium.xcconfig
+  echo "TITANIUM_BASE_SDK2 = '$(TITANIUM_SDK)/iphone/include/TiCore'" >> $MODULE_ROOT/titanium.xcconfig
+  echo "HEADER_SEARCH_PATHS= $(TITANIUM_BASE_SDK) $(TITANIUM_BASE_SDK2)" >> $MODULE_ROOT/titanium.xcconfig
 
 fi
 
