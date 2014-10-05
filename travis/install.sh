c2d00f19e9589fe0c84a4d155a342e9bdf90c1be
Ingo Muschenetz <ingo@muschenetz.com>
2014-10-04 21:08:27 -0700

    Ensure NDK is installed in module folder, not with rest of SDKs

--- travis/install.sh
+++ travis/install.sh
@@ -112,6 +112,7 @@ if [ -d "$MODULE_ROOT/android/" ]; then
   ANDROID_NDK=`ti info -t android -o json | jq -r '.android.ndk.path'`
 
   if [ ! -d "$ANDROID_NDK" ]; then
+    cd "$MODULE_ROOT"
     wget http://dl.google.com/android/ndk/android-ndk-r8c-darwin-x86.tar.bz2
     tar xzf android-ndk-r8c-darwin-x86.tar.bz2
     ANDROID_NDK=${PWD}/android-ndk-r8c
@@ -132,10 +133,10 @@ fi
 if [ -d "$MODULE_ROOT/ios/" ]; then
   # Write out properties file
  
-  echo "TITANIUM_SDK = $TITANIUM_SDK" > titanium.xcconfig
-  echo "TITANIUM_BASE_SDK = \"\$(TITANIUM_SDK)/iphone/include\"" >> titanium.xcconfig
-  echo "TITANIUM_BASE_SDK2 = \"\$(TITANIUM_SDK)/iphone/include/TiCore\"" >> titanium.xcconfig
-  echo "HEADER_SEARCH_PATHS= \$(TITANIUM_BASE_SDK) \$(TITANIUM_BASE_SDK2)" >> titanium.xcconfig
+  echo "TITANIUM_SDK = $TITANIUM_SDK" > $MODULE_ROOT/titanium.xcconfig
+  echo "TITANIUM_BASE_SDK = \"\$(TITANIUM_SDK)/iphone/include\"" >> $MODULE_ROOT/titanium.xcconfig
+  echo "TITANIUM_BASE_SDK2 = \"\$(TITANIUM_SDK)/iphone/include/TiCore\"" >> $MODULE_ROOT/titanium.xcconfig
+  echo "HEADER_SEARCH_PATHS= \$(TITANIUM_BASE_SDK) \$(TITANIUM_BASE_SDK2)" >> $MODULE_ROOT/titanium.xcconfig
 
 fi
 
