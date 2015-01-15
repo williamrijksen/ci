bbb0dd3f9c6b178f7f82399ebb45d9580de2578a
Muhammad Dadu <muhammad@dadu.co>
2015-01-14 17:12:16 -0800

    [TIMODOPEN-466] Add module installation and application build steps to Travis scripts

--- travis/script.sh
+++ travis/script.sh
@@ -6,19 +6,61 @@ STATUS=0
 # If iOS module exists, build
 if [ -d "$MODULE_ROOT/ios/" ]; then
 
-  echo
-  echo "Building iOS version"
-  echo
+	echo
+	echo "Building iOS version"
+	echo
 
-  cd $MODULE_ROOT/ios/
-  cp $MODULE_ROOT/titanium.xcconfig titanium.xcconfig
-  cat titanium.xcconfig
-  ./build.py
+	cd $MODULE_ROOT/ios/
+	cp $MODULE_ROOT/titanium.xcconfig titanium.xcconfig
+	cat titanium.xcconfig
+	./build.py
 
-  let STATUS=$?
-  if (( "$RETSTATUS" == "0" )) && (( "$STATUS" != "0" )); then
-    let RETSTATUS=$STATUS
-  fi
+	let STATUS=$?
+	if (( "$RETSTATUS" == "0" )) && (( "$STATUS" != "0" )); then
+	  	let RETSTATUS=$STATUS
+	else 
+		# Test Module in a Titanium Application
+		echo "Reading module manifest"
+		MODULE_ID=$(sed -n 's/.*moduleid: \([^ ]*\).*/\1/p' manifest)
+		MODULE_VERSION=$(sed -n 's/^version: \([^ ]*\).*/\1/p' manifest)
+		echo "Module Info: " $MODULE_ID '@' $MODULE_VERSION
+
+		#Install module
+		echo "Installing module"
+		ti sdk install $MODULE_ID*
+
+		#This already happens in the previous script
+		#echo "Downloading latest SDK"
+		#ti sdk install
+		#ti sdk select latest
+
+		#Create new application
+		echo "Creating new application"
+		ti create -t app -p ios -d "./TestModule" -n "TestModule" --id "com.appc.TestModule" -u "http://appcelerator.com" --force
+		cd ./TestModule/TestModule
+
+		#Append module to manifest
+		echo "Add module to application"
+		sed -i "" 's/<modules>/&<module version="'$MODULE_VERSION'">'$MODULE_ID'<\/module>/g' tiapp.xml
+
+		#echo "Copying module example"
+		#cp -r $HOME/Library/Application\ Support/Titanium/modules/iphone/$MODULE_ID/$MODULE_VERSION/example/. ./Resources/
+
+		#Append module to app.js
+		cd ./Resources/
+		echo -e "var moduleToTest = require('"$MODULE_ID"');\n$(cat app.js)" > app.js
+		cd ../
+
+		#Build application but do not run simulator
+		echo "Build application"
+		ti build -b -p ios -d "./"
+	
+		let STATUS=$?
+		
+		if (( "$RETSTATUS" == "0" )) && (( "$STATUS" != "0" )); then
+			let RETSTATUS=$STATUS
+		fi
+	fi
 fi
 
 # if Android module exists, build
@@ -34,6 +76,10 @@ if [ -d "$MODULE_ROOT/android/" ]; then
   
   # if lib folder doesn't exist, create it
   mkdir -p lib
+  # if build folder doesn't exist, create it
+  mkdir -p build
+  # if build/docs folder doesn't exist, create it
+  mkdir -p build/docs
 
   ant clean
   ant
