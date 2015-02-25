6db1f6a66f8284af8581fce9ac561814a3095393
Ingo Muschenetz <ingo@muschenetz.com>
2015-02-24 21:19:15 -0800

    Update install-java-osx.sh

--- travis/install-java-osx.sh
+++ travis/install-java-osx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 wget http://support.apple.com/downloads/DL1572/en_US/JavaForOSX2014-001.dmg
-MOUNTDIR=`hdiutil mount JavaForOSX2014-001.dmg -mountroot /Volumes/Java | tail -1 | sed -n 's/.*\(\/Volumes\/Java.*\)/\1/p'`
+MOUNTDIR=`hdiutil mount JavaForOSX2014-001.dmg | tail -1 | sed -n 's/.*\(\/Volumes\/Java.*\)/\1/p'`
 sudo installer -pkg "${MOUNTDIR}/JavaForOSX.pkg" -target /
